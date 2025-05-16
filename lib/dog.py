deacb57e65cf30dd7b7c9002719b61e3b12a16c0
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-15 22:01:01 -0400

    Completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,42 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name=None, breed=None):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+         return self._name
+    
+    def get_breed(self):
+         return self._breed
+
+    def set_name(self, name):
+            if name is None:
+             return
+            if isinstance(name, str) and (1 <= len(name) <= 25):
+                print(f"Setting name to { name }.")
+                self._name = name
+            else:
+                print("Name must be string between 1 and 25 characters.")
+
+    def set_breed(self, breed):
+          if breed is None:
+             return
+          if breed in APPROVED_BREEDS:
+              print(f"Setting breed to { breed }.")
+              self._breed = breed
+          else:
+              print("Breed must be in list of approved breeds.")            
+        
+    name = property(get_name, set_name)
+
+    breed = property(get_breed, set_breed)
+   
+
+    
+            
+
+
     pass
+
